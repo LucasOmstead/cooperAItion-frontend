1152c2c37ca567e0ecd28ce0a53b1d2726e84f84
Lucas Omstead <lucasomstead@gmail.com>
2025-08-04 21:14:49 -0400

    added leaderboard

--- src/app/result-table/result-table.component.ts
+++ src/app/result-table/result-table.component.ts
@@ -20,31 +20,31 @@ import {PastMoves, Player, Defector, Cooperator, GrimTrigger, RandomChooser, Tit
 //res should be stored in 3d array (2d for which players, 1d for res)
 //At the end, display a number in each cell of the table; when the user hovers over the number they can see the match history (stored in table)
 
-export class GameComponent implements OnInit, AfterViewInit {
-  //playerMap stores the number of each type of agent in the environment. Payoffs stores the payoff matrix; payoffs[i][j] = reward for player 1 if he makes action i and p2 makes action j
-  //model stores the model returned from the backend
+export class GameComponent implements OnInit {
   @Input() scores: number[] = [];
   @Input() players: Player[] = [];
   @Input() payoffs: number[][] = [];
-  @Input() matchResults: [number[], number[]][][] = []; //it's pretty nested. matchResults[i][j] = results between those players, which is stored as 2 lists, containing p1 and p2's actions. 
+  @Input() matchResults: [number[], number[]][][] = [];
 
-  @ViewChild('table') table!: ElementRef;
-  @ViewChild('curPlayerName') curPlayerName!: ElementRef;
-
-
-  
+  rankedPlayers: { index: number; score: number }[] = [];
+  selectedPlayer: number | null = null;
   playerFinished = true;
 
-  constructor() {
-  }
+  constructor() {}
 
-  floor = Math.floor;
   ngOnInit(): void {
-    // console.log(this.matchResults);
+    // Create array of player indices and their scores
+    this.rankedPlayers = this.scores.map((score, index) => ({
+      index,
+      score
+    }));
+
+    // Sort by score in descending order
+    this.rankedPlayers.sort((a, b) => b.score - a.score);
   }
 
-  ngAfterViewInit() {
-    this.table.nativeElement.style['grid-template-columns'] = "1fr ".repeat(this.players.length+1);
-    this.table.nativeElement.style['grid-template-rows'] = "1fr ".repeat(this.players.length+1);
+  togglePlayerDetails(rank: number): void {
+    this.selectedPlayer = this.selectedPlayer === rank ? null : rank;
   }
 }
+
